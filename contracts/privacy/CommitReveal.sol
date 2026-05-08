5cf995bdeba01c842afc025e13d2d7d3bc67d0fc
ABHINAV RAJ <rajabhinav734@gmail.com>
2026-05-08 22:07:38 +0000

    feat: implement commit-reveal voting scheme
    Advanced blockchain voting system feature implementation.
    
    Co-authored-by: GitHub Domination Agent <agent@github.com>

--- contracts/privacy/CommitReveal.sol
+++ contracts/privacy/CommitReveal.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
  * @title CommitReveal
  * @dev implement commit-reveal voting scheme
  * @custom:security-contact security@votingdao.org
- * @custom:created 2026-05-08 22:07:33
+ * @custom:created 2026-05-08 22:07:38
  */
 contract CommitReveal is ERC20, AccessControl, ReentrancyGuard {
 
